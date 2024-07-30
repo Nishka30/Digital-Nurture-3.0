ef3487a851326b1be2abac2a349d15b5c404a522
Nishka Shrimali <121378768+Nishka30@users.noreply.github.com>
2024-07-30 22:14:17 +0530

    Update Customer.java

--- Week 1_Design Patterns and Principles/DependencyInjection/Customer.java
+++ Week 1_Design Patterns and Principles/DependencyInjection/Customer.java
@@ -1,5 +1,33 @@
-package Week 1_Design Patterns and Principles.DependencyInjection;
+package DependencyInjection;
 
 public class Customer {
-    
+    private int id;
+    private String name;
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{id=" + id + ", name='" + name + "'}";
+    }
 }
