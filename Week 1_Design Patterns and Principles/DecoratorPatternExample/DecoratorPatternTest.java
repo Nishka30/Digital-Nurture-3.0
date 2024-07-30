f7dd0cdf499544540477d15dc1916cce59c86d27
Nishka Shrimali <121378768+Nishka30@users.noreply.github.com>
2024-07-30 22:11:28 +0530

    Update DecoratorPatternTest.java

--- Week 1_Design Patterns and Principles/DecoratorPatternExample/DecoratorPatternTest.java
+++ Week 1_Design Patterns and Principles/DecoratorPatternExample/DecoratorPatternTest.java
@@ -1,5 +1,11 @@
-package Week 1_Design Patterns and Principles.DecoratorPatternExample;
-
+// DecoratorPatternTest.java
 public class DecoratorPatternTest {
-    
+    public static void main(String[] args) {
+        Notifier emailNotifier = new EmailNotifier();
+        Notifier smsNotifier = new SMSNotifierDecorator(emailNotifier);
+        Notifier slackNotifier = new SlackNotifierDecorator(smsNotifier);
+
+        String message = "Hello, this is a notification!";
+        slackNotifier.send(message);
+    }
 }
